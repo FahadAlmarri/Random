894605ca7b5cca0ded6fad91929033c329f093aa
abok <FahadAlmarri>
2022-12-10 17:25:14 +0300

    comment test.py

--- fahisProject/test.py
+++ fahisProject/test.py
@@ -1,4 +1,4 @@
-from models import User
+#from models import User
 
-p=User.objects.create(user_email="fahd.com",username="abomru",password_hash='fdkljhsk',first_name="fahd",last_name="mri")
-p.save()
+#p=User.objects.create(user_email="fahd.com",username="abomru",password_hash='fdkljhsk',first_name="fahd",last_name="mri")
+#p.save()
